034723f875b3d563baeeb646b98d230e4767ae26
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-07-19 19:52:41 -0400

    Login, AxiosAuth, and PrivateRoute set ups

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
@@ -8,11 +8,15 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <h1>Log into your BubblePage!</h1>
+        <div className="login">
         <Route exact path="/" component={Login} />
+        </div>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+
       </div>
     </Router>
   );
